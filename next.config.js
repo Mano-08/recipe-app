1775c226fa4b4e7447fcec05000d9a7ed4878e3b
Mano <mano.26q@gmail.com>
2023-01-20 23:01:51 +0530

    updated preLoadedData url

--- next.config.js
+++ next.config.js
@@ -8,6 +8,20 @@ const nextConfig = {
     API_ID: process.env.API_ID,
     API_KEY: process.env.API_KEY,
   },
+  images: {
+    domains: ['edamam-product-images.s3.amazonaws.com',
+      'www.seriouseats.com',
+      'i0.wp.com',
+      'honestcooking.com',
+      'hips.hearstapps.com',
+      'www.simplyrecipes.com',
+      'images.immediate.co.uk',
+      'irepo.primecp.com',
+      'whiteonricecouple.com',
+      'imagesvc.meredithcorp.io',
+      'images.food52.com',
+      'www.foodrepublic.com'],
+  },
 };
 
 module.exports = nextConfig;
